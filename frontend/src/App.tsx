80c529bb99162bdad2d253fe64bad42da7710d4b
Taninwat K. (Ice) <60184555+Taninwat-55@users.noreply.github.com>
2025-11-10 03:35:13 +0100

    update: links on nav and sidebar and fix log-out direction (#50)

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,15 +1,12 @@
 import { Outlet } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import { BevisToaster } from "./components/ui/Toast";
-// import FeedbackButton from "./components/ui/FeedbackButton";
 
 export default function App() {
   return (
     <main className="min-h-screen bg-gray-50 text-gray-800">
-      {/* You can add Navbar or Sidebar later */}
       <Navbar />
       <Outlet />
-      {/* <FeedbackButton /> */}
       <BevisToaster />
     </main>
   );
