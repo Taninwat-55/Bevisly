44807ff18c30526232032edb53a957b09c6b8095
Taninwat-55 <60184555+Taninwat-55@users.noreply.github.com>
2026-01-22 17:29:50 +0100

    fix: remove generateLinkedInShareUrl from barrel export

--- frontend/src/components/sharing/index.ts
+++ frontend/src/components/sharing/index.ts
@@ -1,3 +1,3 @@
-export { default as ShareToLinkedIn, generateLinkedInShareUrl } from "./ShareToLinkedIn";
+export { default as ShareToLinkedIn } from "./ShareToLinkedIn";
 export { default as ShareModal } from "./ShareModal";
 export { default as ProfileUrlBadge } from "./ProfileUrlBadge";
